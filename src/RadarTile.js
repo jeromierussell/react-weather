6761d0118c72b97779edeecb719d70beb0b34efe
jeromierussell <jeromierussell@gmail.com>
2020-05-05 14:23:54 -0400

    iphone xr optimization

--- src/RadarTile.js
+++ src/RadarTile.js
@@ -2,12 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export class RadarTile extends React.Component {
-  iframeStyle = {
-    position: 'absolute',
-    top: -80,
-    left: '33%'
-  };
-
   map_url = "https://openweathermap.org/weathermap?basemap=map&cities=false&layer=radar&lat=37.4051&lon=-86.7046&zoom=6";
 
   constructor(props) {
@@ -24,7 +18,7 @@ export class RadarTile extends React.Component {
   render() {
     return (
       <div className="map-container" onClick={this.handleClick}>
-        <iframe title="radar-map" src={this.map_url} width="400" height="800" style={this.iframeStyle}/>
+        <iframe id="radar-map" title="radar-map" src={this.map_url} width="400" height="800"/>
         <div className="map-container-button-area">
           <button className="map-container-button"><span>Back</span></button>
         </div>
